69911ef808f5a554e6b434839a40d41a2210a560
paraj003 <paraj003@gmail.com>
2016-12-30 17:17:50 -0500

    Time-evolution Hamiltonian

--- FloquetHubbard_timeevolution.jl
+++ FloquetHubbard_timeevolution.jl
@@ -6,10 +6,10 @@
 
 module FloquetHubbard_timeevolution
 using TensorOperations
-export g, U_UBar
+export g, U_Ubar
 
 function g(t,T)
-        if rem(t,T)<T/2
+        if rem(t,T)<=T/2
                 return -1.0
         else
                 return 1.0
@@ -17,15 +17,17 @@ function g(t,T)
 end
 
 function U_Ubar(T,t,dt,d,V0)
-#Takes Hamiltonian, and outputs a tuple, with tensors U and Ubar for evolution.
+#Takes Hamiltonian, and outputs a tuple, with tensors U and Ubar fLr evolution.
 #inputs: T: Time period, t:current time, dt: time of evolution, d:local Hilbertspace dimension,V0: interaction potentian
         #define the two-site Hamiltonian
         H2site=[0.0 0.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 1.0 0.0 0.0; 0.0 0.0 0.0 V0*g(t,T)]
-        U2site=expm(-1im*H2site*dt);
+#	H2site=[1.0 0.0 0.0 0.0; 0.0 1.0 0.0 0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0]
+        U2site=expm(1im*H2site*dt);
         #define P from section 7.1.2 Schoolwock,section 8 on systemNotes
         P=zeros(Complex128,4,4);
-        p=[1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4];
-        q=[1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4];
+
+        parr=[1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4];
+        qarr=[1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4];
         s1arr=[0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]+1;#add 1 to make them correspond to physical index
         s2arr=[0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1]+1;
         s1primearr=[0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1]+1;
@@ -33,7 +35,7 @@ function U_Ubar(T,t,dt,d,V0)
         ptilde=[1,1,2,2,1,1,2,2,3,3,4,4,3,3,4,4];
         qtilde=[1,2,1,2,3,4,3,4,1,2,1,2,3,4,3,4];
         for p1=1:16
-                P[p[p1],q[p1]]=U2site[ptilde[p1],qtilde[p1]];
+                P[ptilde[p1],qtilde[p1]]=U2site[parr[p1],qarr[p1]];
         end
         O_SVD=svdfact(P);
         O_U1=O_SVD[:U]*diagm(sqrt(O_SVD[:S]));
@@ -41,8 +43,8 @@ function U_Ubar(T,t,dt,d,V0)
         U=zeros(Complex128,d,d,length(O_SVD[:S]))#have removed the index of dim 1 (sec. 7.1.2)
         Ubar=zeros(Complex128,d,d,length(O_SVD[:S]))#I have removed the index of dimension 1
         for p1=1:16
-                U[s1arr[p1],s1primearr[p1],:]=O_U1[ptilde[p1],:]
-                Ubar[s2arr[p1],s2primearr[p1],:]=O_Ubar1[:,qtilde[p1]]
+                U[s1arr[p1],s1primearr[p1],:]=copy(O_U1[ptilde[p1],:])
+                Ubar[s2arr[p1],s2primearr[p1],:]=copy(O_Ubar1[:,qtilde[p1]])
         end
         return U,Ubar
 end
